f8db488b537bdcc521e980c86bd6abc83249a0dd
tropicdenis <tropicdenis@gmail.com>
2021-04-17 09:44:07 +0300

    React-Kabzda 22 is done. Added Clock component.

---
+++ src/Components/Clock/Clock.tsx
@@ -0,0 +1,28 @@
+import {useEffect, useState} from "react";
+
+type PropsType = {};
+
+const get2digitsString = (num: number) => num < 10 ? "0" + num : num;
+
+export const Clock: React.FC<PropsType> = (props) => {
+    const [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+        const intervalID = setInterval(() => {
+            console.log('tick');
+            setDate(new Date());
+        }, 1000)
+
+        return ()=> {
+            clearInterval(intervalID) // Вызовется один раз, когда умрёт компонента Clock
+        }
+    }, []);
+
+    return <div>
+        <span>{get2digitsString(date.getHours())}</span>
+        :
+        <span>{get2digitsString(date.getMinutes())}</span>
+        :
+        <span>{get2digitsString(date.getSeconds())}</span>
+    </div>
+}
--- src/stories/UseState.stories.tsx
+++ src/stories/UseState.stories.tsx
@@ -1,3 +1,4 @@
+
 import React, {useMemo, useState} from "react";
 
 export default {
